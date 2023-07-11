e0b89c16f9d7f6a0e05ea186dab77cca188896aa
sarin <sarinm101@gmail.com>
2023-07-11 06:03:33 +0530

    Added the image as well as style and handling error

--- routes/index.js
+++ routes/index.js
@@ -9,27 +9,33 @@ router.get('/', function(req, res, next) {
 
 router.post('/search/repo', async (req, res) => {
   const { searchQuery } = req.body;
-  if (!searchQuery) {
+
+  // Check if searchQuery exists
+  if (!(searchQuery.trim())) {
     return res.status(400).send({ status: 'No search query provided' });
   }
-  console.log('search...query////',searchQuery);
+
+  // Construct the URL for the GitHub API search endpoint
   const url = `http://api.github.com/search/repositories?q=${searchQuery}`;
-  console.log('url...',url)
+
   try {
+    // Make a GET request to the GitHub API
     let response = await axios.get(url);
+
+    // Check if the response indicates a service unavailable error (503)
     if (response && response.status === 503) {
-      res.status(500).send({ status:'Internal Server Error', data:[] });
+      res.status(500).send({ status: 'Internal Server Error', data: [] });
     }
+    // Check if the response has valid data with items
     else if (response && response.data && response.data.items) {
-      console.log('response....',response.data)
-      console.log('response....',response.data)
-      res.send({ status:'OK', data:response.data.items });
+      res.send({ status: 'OK', data: response.data.items });
     } else {
-      res.status(404).send({ status:'Not Available', data:[] });
+      // If no valid data found, return a not available response
+      res.status(404).send({ status: 'Not Available', data: [] });
     }
-  } catch (error) { 
-    console.log('error....',error)
-    res.status(500).send({ status: 'Server Error' });
+  } catch (error) {
+    // Handle any errors during the request
+    return res.status(500).send({ status: 'Server Error, Please try again after sometime.' });
   }
 });
 
